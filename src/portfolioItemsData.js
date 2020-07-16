672038a12986b8a495060245636354b2fca05a92
tylergreulich <tgreul6@gmail.com>
2020-07-15 19:06:30 -0700

    updated title

--- src/portfolioItemsData.js
+++ src/portfolioItemsData.js
@@ -1,12 +1,12 @@
 export const portfolioItemsData = [
   {
-    jobTitle: 'Web Developer',
+    jobTitle: 'Full-Stack Web Developer',
     company: 'Yodi',
     companyUrl: 'https://www.yodi.tech/',
     date: 'June 2020 - Present',
     responsibilities: [
       'Created both server-side and statically rendered websites with the React framework Nextjs. Used Redux for state management and Apollo for consuming GraphQL APIs.',
-      'Built GraphQL APIs with Nodejs and modified already existing REST APIs.',
+      'Built GraphQL and REST APIs with Nodejs using JavaScript and TypeScript and modified already existing REST APIs.',
       'Implemented automated testing with Cypress and reduced the amount of time for tests to run by 15%.'
     ]
   }
